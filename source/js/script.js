ae968ff3e5c7a590f8034f56eff80aa020062cea
Яна <1yana05@mail.ru>
2023-04-25 17:46:00 +0300

    вносит исправления

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,12 @@
 let navMain = document.querySelector('.main-nav')
-let navToggle = document.querySelector('.main-nav__toggle')
+let navToggle = document.querySelector('.main-header__toggle')
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed')
     navMain.classList.add('main-nav--opened')
   } else {
-    navMain.classList.remove('main-nav--opened')
-    navMain.classList.add('main-nav--closed')
+    navMain.classList.remove('main-nav--closed')
+    navMain.classList.add('main-nav--opened')
   }
 })
